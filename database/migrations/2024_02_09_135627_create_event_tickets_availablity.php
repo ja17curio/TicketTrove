f5aa00a93934af340f2ca362407baf487dd2b5f3
Mrttog <tigobroers99@gmail.com>
2024-02-09 15:03:24 +0100

    created event tickets availability migration

---
+++ database/migrations/2024_02_09_135627_create_event_tickets_availablity.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('event_tickets_availablity', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->unsignedBigInteger("available");
+            $table->double("price");
+            $table->unsignedBigInteger("ticket_type_id");
+            $table->unsignedBigInteger("event_it");
+
+            $table->foreign("event_id")->references("id")->on("events");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('event_tickets_availablity');
+    }
+};
