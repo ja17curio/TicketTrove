2e232151b7dd218a7fcfb72b3df517effa696291
Mrttog <tigobroers99@gmail.com>
2024-03-01 15:11:44 +0100

    removed unused imports

--- database/seeders/TicketTypesSeeder.php
+++ database/seeders/TicketTypesSeeder.php
@@ -3,7 +3,6 @@
 namespace Database\Seeders;
 
 use App\Models\TicketType;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class TicketTypesSeeder extends Seeder
