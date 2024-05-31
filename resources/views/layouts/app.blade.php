376a5bd4c99e1f8bdea5d39a76eab92134f45ae6
Rowen Bieshaar <rowen.04@hotmail.com>
2024-05-31 10:22:53 +0200

    add user setup button

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -56,10 +56,13 @@
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="{{ route('users.edit', AUTH::user()->id)}}"> 
+                                        {{__('Instellingen')}}l
+                                    </a>
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
+                                        {{ __('Uitloggen') }}
                                     </a>
 
                                     <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
